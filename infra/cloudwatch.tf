affacdfb33deaa6ac6210be9ef0934358c9f7e6f
Refayat Haque <refayathaque@ip-192-168-0-5.ec2.internal>
2022-06-22 13:20:04 -0400

    how do I get green squares?

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -3,5 +3,6 @@ resource "aws_cloudwatch_log_group" "hello_world_task" {
   tags = {
     Environment = "dev"
     Application = "hello-world-microservice"
+    Type        = "ECS task container log"
   }
 }
