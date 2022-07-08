4011d0077b134987de46c31eb13f4bc826183e03
Refayat Haque <refayathaque@Refayats-MacBook-Pro-2.local>
2022-07-08 17:45:39 -0400

    Fri Jul 8 545p

---
+++ containers/reads-from-dydb/list-tables.js
@@ -0,0 +1,36 @@
+import { DynamoDBClient, ListTablesCommand } from "@aws-sdk/client-dynamodb"; // ES Modules import
+
+const configDevLocal = {
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/modules/credentials.html
+};
+
+const configDev = {
+  region: "us-east-1",
+};
+
+const client = new DynamoDBClient(
+  process.env.DEV_LOCAL ? configDevLocal : configDev
+);
+
+const params = {};
+// both values are optional as per documentation, so passing an empty object will do
+
+const command = new ListTablesCommand(params);
+
+export default async () => {
+  try {
+    const response = await client.send(command);
+    console.log(response.TableNames);
+    return response.TableNames;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/classes/listtablescommand.html
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/interfaces/listtablescommandinput.html
